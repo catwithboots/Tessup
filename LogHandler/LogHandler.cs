a128499c0dec9c713ae3d1c771742f4d93f2bef5
kmeinster@schubergphilis.com <kmeinster@catwithboots.com>
2015-02-19 02:21:12 +0100

    Changing outputs to lower |TODO: Needs to be configurable|Added branch for metrics

--- LogHandler/LogHandler.cs
+++ LogHandler/LogHandler.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NLog;
-using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using System.Reflection;
